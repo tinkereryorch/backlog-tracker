2f726016b662523bda1a317fbf06990318aa97d5
Jorge Garcia <jegcelorio@yahoo.com>
2023-07-01 23:53:37 -0500

    Removed styles

--- frontend/src/app/game/components/GameTitle.js
+++ frontend/src/app/game/components/GameTitle.js
@@ -1,7 +1,5 @@
 export default function GameTitle({title}) {
     return(
-        <div style={{padding: '10px'}}>
-            <p>Game: {title}</p>
-        </div>
+        <p>Game: {title}</p>
     );
 }
