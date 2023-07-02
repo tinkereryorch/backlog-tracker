1aa70d029cfd4dda6f9f83f3699bf0db9300f65c
Jorge Garcia <jegcelorio@yahoo.com>
2023-07-01 23:49:33 -0500

    Added file with mock data

---
+++ frontend/src/data/mockData.js
@@ -0,0 +1,11 @@
+export const data = [
+    {
+        id: 0,
+        title: 'Tomb Raider',
+        status: 'In Progress',
+        startDate: new Date().getTime(),
+        timeSpent: 2,
+        notes: 'Notes about Tomb Raider',
+        guide: 'Tomb Raider Guide URL'
+    },
+]
