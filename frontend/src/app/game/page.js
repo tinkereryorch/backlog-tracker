cf574be972af0a1f5b5f7e8dfbd7a0a281cf663e
Jorge Garcia <jegcelorio@yahoo.com>
2023-07-02 21:16:19 -0500

    Removed GameTitle reference

--- frontend/src/app/game/page.js
+++ frontend/src/app/game/page.js
@@ -1,10 +1,8 @@
 import GameDetails from "./components/GameDetails";
-import GameTitle from "./components/GameTitle";
 
 export default function Game() {
     return (
         <div className="container">
-            <GameTitle title="MyGame" />
             <GameDetails />
         </div>
     );
