0f8848cb21fcfca9ab9f10078f2b050961bd9cc9
Jorge Garcia <jegcelorio@yahoo.com>
2023-07-02 21:16:38 -0500

    Added gameTitle field

--- frontend/src/app/game/components/GameDetails.js
+++ frontend/src/app/game/components/GameDetails.js
@@ -9,43 +9,50 @@ export default function GameDetails() {
     }
 
     function handleSave(event) {
+
+        console.log(data);
+
         const formData = new FormData(event.target.form);
         const status = formData.get('game_status');
         const startDate = formData.get('start_date');
         const timeSpent = formData.get('time_spent');
         const notes = formData.get('notes');
         const guide = formData.get('guide');
+        const gameTitle = formData.get('game_title');
         const id = data.length;
 
-        const objectData = {id, status, startDate, timeSpent, notes, guide};
+        const objectData = { id, status, startDate, timeSpent, notes, guide, gameTitle };
 
         data.push(objectData);
-
         console.log(data);
     }
 
     return (
         <form onSubmit={handleSubmit} method="post" >
-            <div style={{padding: '10px'}}>
+            <div style={{ padding: '10px' }}>
+                <label htmlFor="game_title">Game Title: </label>
+                <input type="text" id="game_title" name="game_title" style={{ color: 'black' }} />
+            </div>
+            <div style={{ padding: '10px' }}>
                 <label htmlFor="game_status">Status: </label>
                 <select name="game_status" id="game_status" style={{ color: 'black' }} >
                     <option>In Progress</option>
                     <option>Finished</option>
                 </select>
             </div>
-            <div style={{padding: '10px'}}>
+            <div style={{ padding: '10px' }}>
                 <label htmlFor="start_date">Start Date: </label>
                 <input type="datetime-local" id="start_date" name="start_date" style={{ color: 'black' }} />
             </div>
-            <div style={{padding: '10px'}}>
+            <div style={{ padding: '10px' }}>
                 <label htmlFor="time_spent">Time spent in hours: </label>
                 <input type="number" id="time_spent" name="time_spent" style={{ color: 'black' }} />
             </div>
-            <div style={{padding: '10px'}}>
+            <div style={{ padding: '10px' }}>
                 <label htmlFor="notes">Notes: </label>
                 <input type="text" id="notes" name="notes" style={{ color: 'black' }} />
             </div>
-            <div style={{padding: '10px'}}>
+            <div style={{ padding: '10px' }}>
                 <label htmlFor="guide">Guide: </label>
                 <input type="text" id="guide" name="guide" style={{ color: 'black' }} />
             </div>
