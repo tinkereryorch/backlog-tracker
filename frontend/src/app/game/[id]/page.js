784c1fbb2eb3a301f30aac3a656401264cab4f7b
Jorge Garcia <jegcelorio@yahoo.com>
2023-07-04 20:23:32 -0500

    Added dynamic route to view/edit a game

---
+++ frontend/src/app/game/[id]/page.js
@@ -0,0 +1,7 @@
+export default function Game({ params }) {
+    return(
+        <div>
+            <p>Edit/View Game: {params.id} Page</p>
+        </div>
+    )
+}
--- frontend/src/app/game/page.js
+++
@@ -1,7 +0,0 @@
-export default function Game() {
-    return(
-        <div>
-            <p>Edit/View Game Page</p>
-        </div>
-    )
-}
