345fa57bdec9b014c4c3e77d9fbf81e9e7c29039
Jorge Garcia <jegcelorio@yahoo.com>
2023-07-04 20:00:56 -0500

    Added rating field

--- frontend/src/data/mockData.js
+++ frontend/src/data/mockData.js
@@ -6,7 +6,7 @@ export const data = [
         startDate: new Date().getTime(),
         timeSpent: 2,
         notes: 'Notes about Tomb Raider',
-        rating: 4
+        rating: null,
     },
     {
         id: 1,
@@ -14,6 +14,15 @@ export const data = [
         status: 'In Progress',
         startDate: new Date().getTime(),
         timeSpent: 5,
+        notes: 'Notes about Uncharted 2',
+        rating: null
+    },
+    {
+        id: 2,
+        title: 'Mario Galaxy',
+        status: 'Finished',
+        startDate: new Date().getTime(),
+        timeSpent: 7,
         notes: 'Notes about Uncharted 2',
         rating: 5
     }
