24a71370b0f1e4b02725490382809945a43b6bf1
Jorge Garcia <jegcelorio@yahoo.com>
2023-07-04 20:00:43 -0500

    Added an array of games to be displayed

--- frontend/src/app/games/page.js
+++ frontend/src/app/games/page.js
@@ -1,11 +1,25 @@
 import { data } from "@/data/mockData";
 
 export default function Games() {
-    return(
-        <ul>
-            {data.map((game, index) => (
-                <li key={index}>{game.title}</li>
-            ))}
-        </ul>
+    return (
+        <>
+            <h1>My Games</h1>
+            <ul>
+                {data.map((game) => (
+                    <li key={game.id}>
+                        <div>
+                            <span>Game: {game.title}</span>
+                            <span>Status: {game.status}</span>
+                            <span>Hours Spent: {game.timeSpent}</span>
+                            {game.rating && <span>Rating: {game.rating}</span>}
+                            <button>👁️‍🗨️</button>
+                            <button>✖️</button>            
+                        </div>
+                        <hr></hr>
+                    </li>
+                    
+                ))}
+            </ul>
+        </>
     )
 }
