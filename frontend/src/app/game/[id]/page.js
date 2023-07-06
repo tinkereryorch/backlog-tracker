3094e1149d3b565074640f306099f79ad361dbfd
Jorge Garcia <jegcelorio@yahoo.com>
2023-07-05 19:31:12 -0500

    Added html elements for Edit/View Game

--- frontend/src/app/game/[id]/page.js
+++ frontend/src/app/game/[id]/page.js
@@ -1,7 +1,21 @@
+import Link from 'next/link';
+import { data } from "@/data/mockData";
+
 export default function Game({ params }) {
+    let game = data[params.id];
+    let date = game.startDate;
+
     return(
         <div>
-            <p>Edit/View Game: {params.id} Page</p>
+            <h1>Edit/View Game</h1>
+            <br>
+            </br>
+            <p>Game Title: {game.title}</p>
+            <p>Status: {game.status}</p>
+            <p>Start Date: {date}</p>
+            <p>Time Spent in Hours: {game.timeSpent}</p>
+            {game.rating && <p>Rating: {game.rating}</p>}
+            <Link href="/games"><button>Back</button></Link>
         </div>
     )
 }
