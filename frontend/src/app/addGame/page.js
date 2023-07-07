c518c9ee27c39ee8007f8ad2f50dd666116c4540
Jorge Garcia <jegcelorio@yahoo.com>
2023-07-06 23:02:46 -0500

    Updated addGame font

--- frontend/src/app/addGame/page.js
+++ frontend/src/app/addGame/page.js
@@ -2,7 +2,7 @@ import AddGameForm from "./components/AddGameForm";
 
 export default function Game() {
     return (
-        <div className="container">
+        <div className="container font-sans">
             <AddGameForm />
         </div>
     );
