9645e4ccfdb79423c0c29988aa12bf7d74728711
Jorge Garcia <jegcelorio@yahoo.com>
2023-07-08 11:23:00 -0500

    Updated daisy theme

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html data-theme="synthwave" lang="en">
+    <html data-theme="dracula" lang="en">
       <body>{children}</body>
     </html>
   )
