16164d971511979182c9f61a071685d8f21ba3d0
Jorge Garcia <jegcelorio@yahoo.com>
2023-07-08 11:23:15 -0500

    Fixed html attribute names

--- frontend/src/app/addGame/components/AddGameForm.js
+++ frontend/src/app/addGame/components/AddGameForm.js
@@ -38,11 +38,11 @@ export default function AddGameForm() {
             <section className="my-12">
                 <form className="space-y-10 text-xl font-bold" onSubmit={handleSubmit} method="post">
                     <div className="flex items-center">
-                        <label className="w-36" for="game_title">Game Title:</label>
+                        <label className="w-36" htmlFor="game_title">Game Title:</label>
                         <input className="w-64 input input-bordered max-w-xs" type="text" id="game_title" name="game_title" />
                     </div>
                     <div className="flex items-center">
-                        <label className="w-36" for="game_status">Status:</label>
+                        <label className="w-36" htmlFor="game_status">Status:</label>
                         <select className="w-64 select select-bordered max-w-xs" name="game_status" id="game_status" value={gameStatus} onChange={handleGameStatusChange}>
                             <option className="font-bold">In Progress</option>
                             <option className="font-bold">Finished</option>
@@ -50,25 +50,25 @@ export default function AddGameForm() {
 
                         {gameStatus == 'Finished' && (
                             <div className="mx-10 flex items-center">
-                                <label className="w-36" for="rating">Rating:</label>
+                                <label className="w-36" htmlFor="rating">Rating:</label>
                                 <input className="w-64 input input-bordered max-w-xs" type="text" id="rating" name="rating" />
                             </div>
                         )}
                     </div>
                     <div className="flex items-center">
-                        <label className="w-36" for="start_date">Start Date:</label>
+                        <label className="w-36" htmlFor="start_date">Start Date:</label>
                         <input className="w-64 input input-bordered max-w-xs" type="datetime-local" id="start_date" name="start_date" />
                     </div>
                     <div className="flex items-center">
-                        <label className="w-36" for="time_spent">Time spent:</label>
+                        <label className="w-36" htmlFor="time_spent">Time spent:</label>
                         <input className="w-64 input input-bordered max-w-xs" type="number" id="time_spent" name="time_spent" placeholder="in hours" />
                     </div>
                     <div className="flex items-center">
-                        <label className="w-36" for="notes">Notes:</label>
+                        <label className="w-36" htmlFor="notes">Notes:</label>
                         <textarea className="textarea textarea-bordered w-full" type="text" id="notes" name="notes"></textarea>
                     </div>
                     <div className="flex justify-between">
-                        <button class="btn btn-outline btn-success" type="submit" onClick={handleSave}>Save</button>
+                        <button className="btn btn-outline btn-success" type="submit" onClick={handleSave}>Save</button>
                         <Link href="/games" passHref ><button className="btn btn-outline btn-error">Cancel</button></Link>
                     </div>
                 </form>
